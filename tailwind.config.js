ea230d61a5ec5c321f20b0147f1d107f12db83c3
Yonaiker Ocando <yonaiker2410@gmail.com>
2022-05-24 16:38:40 -0400

    .

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#f3c614",
+        secondary: "#353535",
+      },
+      height: {
+        80: "20rem",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
